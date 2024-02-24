56abcd046b6a84128ace97ece34eacae34a00f26
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-24 15:41:26 +0300

    microtask map lesson

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 type NewComponentType = {
-    students: any
+    students: StudentType[]
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
 }
 
 export const NewComponent = (props: NewComponentType) => {
+    const mappedStudents = props.students.map(s=>{
+        return(
+            <li key={s.id}>
+                <span>{s.name} </span>
+                <span>{s.age} </span>
+            </li>
+        )
+    })
     return (
-        <div>
-            New Component
-        </div>
+        <ul>
+            {mappedStudents}
+        </ul>
     );
 };
 
