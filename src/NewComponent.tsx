34629ab6808de8e30f890328365ba84b9eea6345
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-24 15:34:41 +0300

    start microtask map

---
+++ src/NewComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type NewComponentType = {
+    students: any
+}
+
+export const NewComponent = (props: NewComponentType) => {
+    return (
+        <div>
+            New Component
+        </div>
+    );
+};
+
